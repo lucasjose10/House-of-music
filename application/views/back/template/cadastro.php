cba75162fca9e77e764a2c217c9648abd50dfc3e
GABRIELROLAND <32078927+GABRIELROLAND@users.noreply.github.com>
2018-10-19 21:40:42 -0300

    mudança no formulario
    inclusão de mais dados

--- application/views/back/template/cadastro.php
+++ application/views/back/template/cadastro.php
@@ -5,14 +5,13 @@ psible content -->
 
 <br>
 
-<center> <p class="font-weight-light  "><h1 class="text-light-primary">Area administrativa</p></h1></center>
 
 
 	<!-- Card -->
 
 	<div class="container  h-100" id="card">
 
-<div class="row h-100 justify-content-center align-items-center">
+<div class="row h-200 justify-content-center -items-center">
 
 <div class="card my-5 animated zoomIn"  style="width: 30rem;" >
 
@@ -40,26 +39,69 @@ psible content -->
 					<!-- Material input password -->
 					<div class="md-form">
 							<i class="fa fa-lock prefix grey-text"></i>
-							<input type="text" id="curso" class="form-control">
+							<input type="text" id="curso" name="curso" class="form-control">
 							<label for="curso" class="font-weight-light">Curso</label>
 					</div>
 					<div class="md-form">
 							<i class="fa fa-lock prefix grey-text"></i>
-							<input type="number" id="valor" class="form-control">
-							<label for="valor" class="font-weight-light">valor</label>
+							<input type="text" id="datan" name="datan" class="form-control">
+							<label for="datan" class="font-weight-light">Data de Nascimento</label>
 					</div>
 					<div class="md-form">
 							<i class="fa fa-lock prefix grey-text"></i>
-							<input type="text" id="endereço" class="form-control">
+							<input type="text" id="vencimento" name="vencimento" class="form-control">
+							<label for="vencimento" class="font-weight-light">Vencimento</label>
+					</div>
+					<div class="md-form">
+							<i class="fa fa-lock prefix grey-text"></i>
+							<input type="text" id="mae" name="mae" class="form-control">
+							<label for="mae" class="font-weight-light">Mãe</label>
+					</div>
+					<div class="md-form">
+							<i class="fa fa-lock prefix grey-text"></i>
+							<input type="text" id="pai" name="pai" class="form-control">
+							<label for="pai" class="font-weight-light">Pai</label>
+					</div>
+					<div class="md-form">
+							<i class="fa fa-lock prefix grey-text"></i>
+							<input type="text" id="endereço" name="endereço" class="form-control">
 							<label for="endereço" class="font-weight-light">endereço</label>
 					</div>
 					<div class="md-form">
 							<i class="fa fa-lock prefix grey-text"></i>
-							<input type="text" id="Professor" class="form-control">
+							<input type="number" id="valor" name="valor" class="form-control">
+							<label for="valor" class="font-weight-light">valor</label>
+					</div>
+					<div class="md-form">
+							<i class="fa fa-lock prefix grey-text"></i>
+							<input type="text" id="bairro" name="bairro" class="form-control">
+							<label for="bairro" class="font-weight-light">Bairro</label>
+					</div>
+					<div class="md-form">
+							<i class="fa fa-lock prefix grey-text"></i>
+							<input type="text" id="cidade" name="cidade" class="form-control">
+							<label for="cidade" class="font-weight-light">Cidade</label>
+					</div>
+					<div class="md-form">
+							<i class="fa fa-lock prefix grey-text"></i>
+							<input type="text" id="Professor" name="Professor" class="form-control">
 							<label for="Professor" class="font-weight-light">Professor</label>
 					</div>
-
-
+					<div class="md-form">
+							<i class="fa fa-lock prefix grey-text"></i>
+							<input type="text" id="dia" name="dia" class="form-control">
+							<label for="dia" class="font-weight-light">Dia</label>
+					</div>
+					<div class="md-form">
+							<i class="fa fa-lock prefix grey-text"></i>
+							<input type="number" id="hora" name="hora" class="form-control">
+							<label for="hora" class="font-weight-light">Hora</label>
+					</div>
+					<div class="md-form">
+							<i class="fa fa-lock prefix grey-text"></i>
+							<input type="number" id="telefone" name="telefone" class="form-control">
+							<label for="telefone" class="font-weight-light">Telefone</label>
+					</div>
 
 					<div class="text-center mt-4">
 					<button class="btn btn-primary" type="submit">cadastrar</button><br><br>
