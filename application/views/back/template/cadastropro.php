5cff2bc04015f5d1134422b6c54fe8d319f1fb78
Lucas José <32078930+lucasjose10@users.noreply.github.com>
2018-10-30 15:45:32 -0300

    cadastro professor view

---
+++ application/views/back/template/cadastropro.php
@@ -0,0 +1,64 @@
+
+
+</nav>
+<!--/.Navbar-->
+
+<br>
+
+
+
+	<!-- Card -->
+
+
+
+
+			<?php
+			echo $this->session->flashdata('sucesso');
+			echo $this->session->flashdata('erro');
+			echo validation_errors();
+			echo form_open('Cadastropro/cadastra_professor');
+			?>
+
+			<center><p> Cadastro Professor </p></center>
+			<div class="row mx-2">
+			    <div class="col-lg-12">
+			        <div class="row">
+			          <div class="col-md-6">
+			            <div class="form-group">
+			              <p align="left">Nome
+			              <input class="form-control" name="nome" type="text" placeholder="Digite aqui o seu nome" pattern=".{3,100}" required>
+			              </p>
+			            </div>
+			            <div class="form-group">
+			            <p align="left">curso
+			              <input class="form-control" name="curso" type="text" placeholder="disciplia que o professor da">
+			              </p>
+			            </div>
+
+										 </p>
+			            </div>
+			          </div>
+			          <div class="col-lg-12 text-center">
+			            <input type="submit" value="Enviar" class="btn btn-primary btn-xl mt-0"  />
+			            </form>    </div>
+			  </div>
+
+
+
+
+
+					<div class="modal-footer">
+												<div class="options font-weight-light">
+
+
+												</div>
+										</div>
+	</div>
+	<!-- Card body -->
+
+</div>
+</div>
+</div>
+			<?php
+			echo form_close();
+			?>
